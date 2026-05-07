be7100a5f16681707f4000ca84061d3ecd4f71ed
Todor Todorovski <todor.todorovski@students.finki.ukim.mk>
2026-05-07 10:12:26 +0200

    Changed files

---
+++ out/build/GCC 13.3.0 x86_64-linux-gnu/CMakeFiles/VS-Code-Projects.dir/DependInfo.cmake
@@ -0,0 +1,35 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/todor/VS-Code-Projects/Exercise-1/main.cpp" "CMakeFiles/VS-Code-Projects.dir/Exercise-1/main.cpp.o" "gcc" "CMakeFiles/VS-Code-Projects.dir/Exercise-1/main.cpp.o.d"
+  "/home/todor/VS-Code-Projects/Exercise-10/10.cpp" "CMakeFiles/VS-Code-Projects.dir/Exercise-10/10.cpp.o" "gcc" "CMakeFiles/VS-Code-Projects.dir/Exercise-10/10.cpp.o.d"
+  "/home/todor/VS-Code-Projects/Exercise-11/11.cpp" "CMakeFiles/VS-Code-Projects.dir/Exercise-11/11.cpp.o" "gcc" "CMakeFiles/VS-Code-Projects.dir/Exercise-11/11.cpp.o.d"
+  "/home/todor/VS-Code-Projects/Exercise-12/12.cpp" "CMakeFiles/VS-Code-Projects.dir/Exercise-12/12.cpp.o" "gcc" "CMakeFiles/VS-Code-Projects.dir/Exercise-12/12.cpp.o.d"
+  "/home/todor/VS-Code-Projects/Exercise-2/2.cpp" "CMakeFiles/VS-Code-Projects.dir/Exercise-2/2.cpp.o" "gcc" "CMakeFiles/VS-Code-Projects.dir/Exercise-2/2.cpp.o.d"
+  "/home/todor/VS-Code-Projects/Exercise-3/3.cpp" "CMakeFiles/VS-Code-Projects.dir/Exercise-3/3.cpp.o" "gcc" "CMakeFiles/VS-Code-Projects.dir/Exercise-3/3.cpp.o.d"
+  "/home/todor/VS-Code-Projects/Exercise-4/4.cpp" "CMakeFiles/VS-Code-Projects.dir/Exercise-4/4.cpp.o" "gcc" "CMakeFiles/VS-Code-Projects.dir/Exercise-4/4.cpp.o.d"
+  "/home/todor/VS-Code-Projects/Exercise-5/5-1.cpp" "CMakeFiles/VS-Code-Projects.dir/Exercise-5/5-1.cpp.o" "gcc" "CMakeFiles/VS-Code-Projects.dir/Exercise-5/5-1.cpp.o.d"
+  "/home/todor/VS-Code-Projects/Exercise-5/5.cpp" "CMakeFiles/VS-Code-Projects.dir/Exercise-5/5.cpp.o" "gcc" "CMakeFiles/VS-Code-Projects.dir/Exercise-5/5.cpp.o.d"
+  "/home/todor/VS-Code-Projects/Exercise-6/6.cpp" "CMakeFiles/VS-Code-Projects.dir/Exercise-6/6.cpp.o" "gcc" "CMakeFiles/VS-Code-Projects.dir/Exercise-6/6.cpp.o.d"
+  "/home/todor/VS-Code-Projects/Exercise-7/7.cpp" "CMakeFiles/VS-Code-Projects.dir/Exercise-7/7.cpp.o" "gcc" "CMakeFiles/VS-Code-Projects.dir/Exercise-7/7.cpp.o.d"
+  "/home/todor/VS-Code-Projects/Exercise-8/8.cpp" "CMakeFiles/VS-Code-Projects.dir/Exercise-8/8.cpp.o" "gcc" "CMakeFiles/VS-Code-Projects.dir/Exercise-8/8.cpp.o.d"
+  "/home/todor/VS-Code-Projects/Exercise-9/9.cpp" "CMakeFiles/VS-Code-Projects.dir/Exercise-9/9.cpp.o" "gcc" "CMakeFiles/VS-Code-Projects.dir/Exercise-9/9.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
